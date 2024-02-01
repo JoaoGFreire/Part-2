011bcad4d381cc42fce9e26c202be22a51faabf5
frederej <frederej@OA-S141-09>
2024-02-01 10:25:54 -0500

    Plane is moving

--- Assets/Week 4/Scripts/Plane.cs
+++ Assets/Week 4/Scripts/Plane.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class Plane : MonoBehaviour
 {
     public List<Vector2> points;
-    public float newPositionThreshold = 0.01f;
+    public float newPositionThreshold = 0.2f;
     Vector2 lastPosition;
+    Vector2 currentPosition;
+    Rigidbody2D rb;
+    public float speed = 1.0f;
 
     LineRenderer lineRenderer;
 
@@ -17,6 +20,36 @@ public class Plane : MonoBehaviour
         lineRenderer.positionCount = 1;
         lineRenderer.SetPosition(0,transform.position);
 
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+   private void FixedUpdate()
+    {
+        currentPosition = transform.position;
+        if(points.Count > 0)
+        {
+            Vector2 direction = points[0] - currentPosition;
+            float angle = Mathf.Atan2(direction.x, direction.y) * Mathf.Rad2Deg;
+            rb.rotation = -angle;
+        }
+        rb.MovePosition(rb.position + (Vector2)transform.up * speed * Time.deltaTime);
+    }
+    private void Update()
+    {
+        lineRenderer.SetPosition(0, transform.position);
+        if (points.Count > 0)
+        {
+            if(Vector2.Distance(currentPosition, points[0])< newPositionThreshold) 
+            {
+                points.RemoveAt(0);
+
+                for(int i = 0; i < lineRenderer.positionCount - 2; i++)
+                {
+                    lineRenderer.SetPosition(i, lineRenderer.GetPosition(i + 1));
+                }
+                lineRenderer.positionCount--;
+            }
+        }
     }
 
     private void OnMouseDown()

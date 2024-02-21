d3eb7ef5bff1a36c993d3d48e8b055e6ccae50eb
PC-JOAO\joaog <joaogfff@gmail.com>
2024-02-21 11:57:31 -0500

    Arrows correctly rotate arrow game object on button press

---
+++ Assets/Assignment/Scripts/Rotate.cs
@@ -0,0 +1,36 @@
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public GameObject arrow;
+    public float rspeed = 50f;
+    bool rotateR = false;
+    bool rotateL = false;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rotateR)
+        {
+            arrow.transform.Rotate(0,0,  -rspeed * Time.deltaTime);
+        }
+        if (rotateL)
+        {
+            arrow.transform.Rotate(0,0,rspeed * Time.deltaTime);
+        }
+    }
+    public void RotateRight()
+    {
+        rotateR = !rotateR;
+    }
+    public void RotateLeft()
+    {
+        rotateL = !rotateL;
+    }
+}

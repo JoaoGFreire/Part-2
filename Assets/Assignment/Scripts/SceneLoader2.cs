a496c0b8c7b44fb6631a9875d2ccb235326be346
PC-JOAO\joaog <joaogfff@gmail.com>
2024-02-21 10:39:26 -0500

    Feature 1 Complete, Main menu has button that changes scenes to main gameplay scene

--- Assets/Assignment/Scripts/SceneLoader2.cs
+++ Assets/Assignment/Scripts/SceneLoader2.cs
@@ -3,18 +3,14 @@ using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader2 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadScene()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        int SceneCurrent = SceneManager.GetActiveScene().buildIndex;
+        int SceneNext = (SceneCurrent + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(SceneNext);
     }
 }
